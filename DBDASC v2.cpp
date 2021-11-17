dc004c6d94570c9dba86f572cd3a745c7ff46627
InkAurora <marthus2001@gmail.com>
2021-11-17 06:24:19 -0300

    Code refactor
    - Now using computer vision to detect skill checks;
    - Some performance improvement;

--- DBDASC v2.cpp
+++ DBDASC v2.cpp
@@ -1,297 +1,378 @@
-#include <iostream>
-#include <thread>
-#include <future>
-#include <strsafe.h>
-#include <windows.h>
+#include <string>
+#include <fstream>
 #include <math.h>
-#include <stdlib.h>
 
-#define ASYNC 1
+#include "ImageSearch.h"
 
 using namespace std;
 
-typedef struct {
-	COLORREF allStr;
-	int r;
-	int g;
-	int b;
-} PDATA;
 
-typedef struct {
-	int x;
-	int y;
-} ROUND;
-
-typedef struct {
-	HDC hdc, hdcTemp;
-	BYTE * bitPointer;
-	int MAX_WIDTH, MAX_HEIGHT;
-	BITMAPINFO bitmap;
-	HBITMAP hBitmap2;
-	void doMagic() {
-		hdc = GetDC(NULL);
-		//GetWindowRect(FindWindowA(NULL, NULL), &rect);
-		MAX_WIDTH = 1600;
-		MAX_HEIGHT = 900;
-
-		bitmap.bmiHeader.biSize = sizeof(bitmap.bmiHeader);
-		bitmap.bmiHeader.biWidth = MAX_WIDTH;
-		bitmap.bmiHeader.biHeight = MAX_HEIGHT;
-		bitmap.bmiHeader.biPlanes = 1;
-		bitmap.bmiHeader.biBitCount = 32;
-		bitmap.bmiHeader.biCompression = BI_RGB;
-		bitmap.bmiHeader.biSizeImage = MAX_WIDTH * 4 * MAX_HEIGHT;
-		bitmap.bmiHeader.biClrUsed = 0;
-		bitmap.bmiHeader.biClrImportant = 0;
-
-		hBitmap2 = CreateDIBSection(hdcTemp, &bitmap, DIB_RGB_COLORS, (void**)(&bitPointer), NULL, 0);
-		hdcTemp = CreateCompatibleDC(hdc);
-		SelectObject(hdcTemp, hBitmap2);
-		DeleteObject(hBitmap2);
-
-		BitBlt(hdcTemp, 0, 0, MAX_WIDTH, MAX_HEIGHT, hdc, 0, 0, SRCCOPY);
-		return;
-	}
-	void undoMagic() {
-		DeleteObject(hdcTemp);
-		ReleaseDC(NULL, hdcTemp);
-		DeleteObject(hdc);
-		ReleaseDC(NULL, hdc);
-		DeleteObject(bitPointer);
-		return;
-	}
-	void refreshMagic() {
-		BitBlt(hdcTemp, 0, 0, MAX_WIDTH, MAX_HEIGHT, hdc, 0, 0, SRCCOPY);
-		return;
-	}
-} BMP;
-
-typedef struct tagTOOLINFO {
-	UINT      cbSize;
-	UINT      uFlags;
-	HWND      hwnd;
-	UINT_PTR  uId;
-	RECT      rect;
-	HINSTANCE hinst;
-	LPTSTR    lpszText;
-#if (_WIN32_IE >= 0x0300)
-	LPARAM lParam;
-#endif
-} TOOLINFO, NEAR *PTOOLINFO, FAR *LPTOOLINFO;
 
 LARGE_INTEGER start, stop, freq;
-double radOffset = 0.15;
+double radOffset;
+int widthHalf = GetSystemMetrics(SM_CXSCREEN) / 2;
+int heightHalf = GetSystemMetrics(SM_CYSCREEN) / 2;
+int radius = 0;
+int xS, yS;
+int bmpPadding = 5;
+int bmpSize, bmpHalfSize;
 
-PDATA GetPixelData(HDC hdc, int x, int y) {
-	PDATA pixelData;
-	pixelData.allStr = GetPixel(hdc, x, y);
-	pixelData.b = GetBValue(pixelData.allStr);
-	pixelData.g = GetGValue(pixelData.allStr);
-	pixelData.r = GetRValue(pixelData.allStr);
-	return pixelData;
-}
 
-PDATA GetBmpData(BMP bmp, int x, int y) {
-	int i = ((bmp.MAX_WIDTH * (bmp.MAX_HEIGHT - y) * 4) + (x * 4)) - 4;
-	PDATA pixel;
-	pixel.b = (int)bmp.bitPointer[i];
-	pixel.g = (int)bmp.bitPointer[i + 1];
-	pixel.r = (int)bmp.bitPointer[i + 2];
-	return pixel;
+
+typedef struct {
+  int b;
+  int g;
+  int r;
+} PDATA;
+
+typedef struct BMP_D {
+  HDC hdc, hdcTemp;
+  BITMAPINFO bitmapInfo;
+  HBITMAP hBitmap;
+  BYTE* bitPointer;
+  int BIT_ALLOC;
+  int MAP_SIZE = 0;
+
+  void CreateBMP(int left = 0, int top = 0, int width = 0, int height = 0) {
+    hdc = GetDC(NULL);
+    if (!hdc) return;
+    //GetWindowRect(FindWindowA(NULL, NULL), &rect);
+
+    BIT_ALLOC = 4;
+
+    MAP_SIZE = width * height * BIT_ALLOC;
+
+    bitmapInfo.bmiHeader.biSize = sizeof(bitmapInfo.bmiHeader);
+    bitmapInfo.bmiHeader.biWidth = width;
+    bitmapInfo.bmiHeader.biHeight = -height;
+    bitmapInfo.bmiHeader.biPlanes = 1;
+    bitmapInfo.bmiHeader.biBitCount = 8 * BIT_ALLOC;
+    bitmapInfo.bmiHeader.biCompression = BI_RGB;
+    bitmapInfo.bmiHeader.biSizeImage = MAP_SIZE;
+    bitmapInfo.bmiHeader.biClrUsed = 0;
+    bitmapInfo.bmiHeader.biClrImportant = 0;
+
+    hBitmap = CreateDIBSection(hdc, &bitmapInfo, DIB_RGB_COLORS, (void**)(&bitPointer), NULL, 0);
+
+    if (!hBitmap) return;
+
+    hdcTemp = CreateCompatibleDC(NULL);
+
+    if (!hdcTemp) return;
+
+    SelectObject(hdcTemp, hBitmap);
+
+    BitBlt(hdcTemp, 0, 0, width, height, hdc, left - (width / 2), top - (height / 2), SRCCOPY);
+
+    return;
+  }
+
+  void DeleteBMP() {
+    DeleteObject(hBitmap);
+    ReleaseDC(NULL, hdc);
+    DeleteDC(hdc);
+    DeleteDC(hdcTemp);
+
+    return;
+  }
+
+  void UpdateBMP() {
+    BitBlt(hdcTemp, 0, 0, bmpSize, bmpSize, hdc, xS - radius - bmpPadding, yS - radius - bmpPadding, SRCCOPY);
+
+    return;
+  }
+
+  void svBMP() {
+    BITMAPFILEHEADER bmfh;
+    int nBitsOffset = sizeof(bmfh) + bitmapInfo.bmiHeader.biSize;
+    LONG lImageSize = bitmapInfo.bmiHeader.biSizeImage;
+    LONG lFileSize = nBitsOffset + lImageSize;
+    bmfh.bfType = 'B' + ('M' << 8);
+    bmfh.bfOffBits = nBitsOffset;
+    bmfh.bfSize = lFileSize;
+    bmfh.bfReserved1 = bmfh.bfReserved2 = 0;
+
+    string szFilePath("C:\\Rules\\bmp.bmp");
+    std::ofstream pFile(szFilePath, std::ios_base::binary);
+
+    pFile.write((const char*)&bmfh, sizeof(BITMAPFILEHEADER));
+    UINT nWrittenFileHeaderSize = pFile.tellp();
+
+    pFile.write((const char*)&bitmapInfo.bmiHeader, sizeof(BITMAPINFOHEADER));
+    UINT nWrittenInfoHeaderSize = pFile.tellp();
+
+    const char* p = reinterpret_cast<const char*>(bitPointer);
+    pFile.write(&p[0], MAP_SIZE);
+    UINT nWrittenDIBDataSize = pFile.tellp();
+    pFile.close();
+
+    return;
+  }
+} BMP_D;
+
+class Pixel {
+public:
+  int x;
+  int y;
+  int r;
+  int g;
+  int b;
+
+  Pixel(double rad, BMP_D bmp) {
+    GetCoords(rad);
+    GetPixelData(bmp);
+
+    return;
+  }
+
+private:
+  void GetCoords(double rad) {
+    x = round(cos(rad) * radius + bmpHalfSize );
+    y = round(sin(rad) * radius + bmpHalfSize);
+
+    return;
+  }
+
+  void GetPixelData(BMP_D bmp) {
+    int i = bmp.bitmapInfo.bmiHeader.biWidth * y * 4 + x * 4;
+
+    b = (int)bmp.bitPointer[i];
+    g = (int)bmp.bitPointer[i + 1];
+    r = (int)bmp.bitPointer[i + 2];
+
+    return;
+  }
+};
+
+
+
+double QPC(int mode) {
+  if (mode) {
+    QueryPerformanceFrequency(&freq);
+    QueryPerformanceCounter(&start);
+  } else if (!mode) {
+    QueryPerformanceCounter(&stop);
+    LONGLONG diff = stop.QuadPart - start.QuadPart;
+    double duration = (double)diff * 1000 / (double)freq.QuadPart;
+
+    return round(duration);
+  }
+
+  return 0;
 }
 
 void SendKey(UINT key) {
-	INPUT input = { 0 };
-	input.type = INPUT_KEYBOARD;
-	input.ki.wVk = key;
-	input.ki.dwFlags = 0;
-	SendInput(1, &input, sizeof(input));
-	Sleep(20);
-	input.ki.dwFlags = KEYEVENTF_KEYUP;
-	SendInput(1, &input, sizeof(input));
-	return;
+  INPUT input = { 0 };
+  input.type = INPUT_KEYBOARD;
+  input.ki.wVk = key;
+  input.ki.dwFlags = 0;
+  SendInput(1, &input, sizeof(input));
+  input.ki.dwFlags = KEYEVENTF_KEYUP;
+  SendInput(1, &input, sizeof(input));
+  return;
 }
 
-ROUND calcCoords(double radian) {
-	ROUND coords;
-	coords.x = round(cos(radian) * 54 + 800);
-	coords.y = round(sin(radian) * 54 + 450);
-	return coords;
+void GetRadius(BMP_D bmp) {
+  int r, g, b;
+
+  auto Compare = [&]() {
+    if (r > 141 and r < 151 and g > 152 and g < 162 and b > 166 and b < 176) return 1;
+    return 0;
+  };
+
+  auto GetBmpData = [&](int x, int y) {
+    int i = bmp.bitmapInfo.bmiHeader.biWidth * y * 4 + x * 4;
+    b = (int)bmp.bitPointer[i];
+    g = (int)bmp.bitPointer[i + 1];
+    r = (int)bmp.bitPointer[i + 2];
+    return;
+  };
+
+  int half = bmp.bitmapInfo.bmiHeader.biWidth / 2;
+
+  for (int i = 40; i < 150; i++) {
+    GetBmpData(half, half - i);
+    if (Compare()) {
+      radius = i;
+      break;
+    }
+
+    GetBmpData(half + i, half);
+    if (radius == 0 and Compare()) {
+      radius = i;
+      break;
+    }
+
+    GetBmpData(half, half + i);
+    if (radius == 0 and Compare()) {
+      radius = i;
+      break;
+    }
+
+    GetBmpData(half - i, half);
+    if (radius == 0 and Compare()) {
+      radius = i;
+      break;
+    }
+  }
+
+  return;
 }
 
-double QPC(int mode) {
-	if (mode) {
-		QueryPerformanceFrequency(&freq);
-		QueryPerformanceCounter(&start);
-	}
-	else if (!mode) {
-		QueryPerformanceCounter(&stop);
-		LONGLONG diff = stop.QuadPart - start.QuadPart;
-		double duration = (double)diff * 1000 / (double)freq.QuadPart;
-
-		return duration;
-	}
-	return 0;
+int FindWhiteSpot(BMP_D bmp, double rad) {
+  Pixel pixel(rad, bmp);
+  if (pixel.r >= 220 and pixel.g >= 220 and pixel.b >= 220) {
+    return 1;
+  }
+  
+  return 0;
 }
 
-int findWhiteSpot(BMP bmp, double radian) {
-	ROUND coords = calcCoords(radian);
-	PDATA pixel = GetBmpData(bmp, coords.x, coords.y);
-	if (pixel.r >= 200 and pixel.g >= 200 and pixel.b >= 200) {
-		return 1;
-	}
-	else {
-		return 0;
-	}
+int FindRedSpot(BMP_D bmp,double rad) {
+  Pixel pixel(rad, bmp);
+  if (pixel.r >= 130 and pixel.g <= 30 and pixel.b <= 30) {
+    return 1;
+  }
+  
+  return 0;
 }
 
-int findRedSpot(BMP bmp,double radian) {
-	ROUND coords = calcCoords(radian);
-	PDATA pixel = GetBmpData(bmp, coords.x, coords.y);
-	if (pixel.r >= 130 and pixel.g <= 30 and pixel.b <= 30) {
-		return 1;
-	}
-	else {
-		return 0;
-	}
+int WhitePart(BMP_D bmp, double& rad) {
+  QPC(1);
+
+  cout << "Locating white spot" << endl;
+
+  for (int j = 0; j < 5; j++) {
+    rad = 0;
+
+    bmp.UpdateBMP();
+
+    for (int i = 0; i < 600; i++) {
+      if (FindWhiteSpot(bmp, rad)) {
+        cout << "White spot found at: " << rad << "  on attempt: " << j << ". Took: " << QPC(0) << " milliseconds" << endl;
+        return 1;
+      }
+
+      rad += 0.01;
+    }
+  }
+
+  cout << "White spot not found" << endl;
+
+  return 0;
 }
 
-int whitePart(BMP bmp, double * rad) {
-	future<int> fReturn[1000];
-	double fRad[1000];
-	QPC(1);
+int MapNeedle(BMP_D bmp, double rad, double radRed) {
+  cout << "Mapping red needle" << endl;
 
-	cout << "finding white" << endl;
+  while (1) {
+    QPC(1);
 
-	for (int j = 0; j < 5; j++) {
-		*rad = -0.5;
+    bmp.UpdateBMP();
 
-		bmp.refreshMagic();
+    for (int i = 0; i < 100; i++) {
+      if (FindRedSpot(bmp, radRed)) {
+        if (radRed >= rad - radOffset) {
+          cout << "Mapping successful" << endl;
+          return 1;
+        } else {
+          break;
+        }
+      } else {
+        if (i == 99) {
+          cout << "Failed at mapping red needle" << endl;
+          return 0;
+        }
+      }
 
-		for (int i = 0; i < 500; i++) {
-			fReturn[i] = async(launch::async, findWhiteSpot, bmp, *rad);
-			fRad[i] = *rad;
-			*rad += 0.01;
-		}
-
-		for (int i = 0; i < 500; i++) {
-			if (fReturn[i].get()) {
-				*rad = fRad[i];
-				cout  << j << "White spot found at: " << *rad << endl;
-				cout << QPC(0) << "ms" << endl;
-				return 1;
-			}
-		}
-	}
-
-	return 0;
+      radRed += 0.01;
+    }
+  }
 }
 
-int mapNeedle(BMP bmp, double rad, double radRed) {
-	future<int> fReturn[1000];
-	double fRadRed[1000];
+int RedPart(BMP_D bmp, double rad) {
+  double radRed;
 
-	cout << "mapping needle" << endl;
+  cout << "Locating red needle" << endl;
 
-	while (1) {
-		radRed -= 0.5;
+  QPC(1);
 
-		bmp.refreshMagic();
+  for (int j = 0; j < 3; j++) {
+    radRed = -1;
 
-		QPC(1);
+    bmp.UpdateBMP();
 
-		for (int i = 0; i < 100; i++) {
-			fReturn[i] = async(launch::async, findRedSpot, bmp, radRed);
-			fRadRed[i] = radRed;
-			radRed += 0.01;
-		}
+    for (int i = 0; i < 300; i++) {
+      if (FindRedSpot(bmp, radRed)) {
+        cout << "Red needle found at: " << radRed << "  on attempt: " << j << ". Took: " << QPC(0) << " milliseconds" << endl;
 
-		for (int i = 0; i < 100; i++) {
-			if (fReturn[i].get()) {
-				radRed = fRadRed[i];
-				if (abs(radRed - (rad - 0.14)) < radOffset) {
-					return 1;
-				}
-				else { break; }
-			}
-			else { 
-				if (i == 99) {
-					cout << "needle fail" << endl;
-					return 0;
-				} 
-			}
-		}
-	}
+        if (MapNeedle(bmp, rad, radRed)) {
+          return 1;
+        } else {
+          return 0;
+        }
+      }
+
+      radRed += 0.01;
+    }
+  }
+
+  cout << "Red needle not found" << endl;
+    
+  return 0;
 }
 
-int redPart(BMP bmp, double rad) {
-	future<int> fReturn[1000];
-	double fRadRed[1000];
-	double radRed;
 
-	cout << "finding red" << endl;
+int main(int argc, char **argv) {
+  BMP_D bmp;
 
-	for (int j = 0; j < 3; j++) {
-		radRed = -0.5;
+  cout << "Dead by Daylight Auto Skill Check v0.2" << endl;
+  cout << "https://github.com/InkAurora/DBDASC_v2" << endl;
+  if (argc > 1) {
+    radOffset = stod(string(argv[argc - 1]));
+    cout << "Using custom offset: " << radOffset << endl;
+  } else radOffset = 0.12;
+  cout << "============================================================================" << endl;
 
-		bmp.refreshMagic();
+  while (1) {
+    string path(argv[0]);
+    path = path.substr(0, path.find_last_of("\\/"));
+    path += "\\m5btn.bmp";
 
-		QPC(1);
+    int success = ImageSearch(xS, yS, 200, 100, -200, -100, path, 20);
+    xS += 15;
+    yS += 15;
+    if (success == 1) {
+      cout << "Skillcheck found: " << xS << "x " << yS << "y" << endl;
 
-		for (int i = 0; i < 150; i++) {
-			fReturn[i] = async(launch::async, findRedSpot, bmp, radRed);
-			fRadRed[i] = radRed;
-			radRed += 0.01;
-		}
+      double rad;
 
-		for (int i = 0; i < 150; i++) {
-			if (fReturn[i].get()) {
-				radRed = fRadRed[i];
-				cout << j << "red done " << QPC(0) << "ms" << endl;
-				if (mapNeedle(bmp, rad, radRed)) {
-					return 1;
-				}
-				else { return  0; }
-			}
-		}
-	}
-	
-	return 0;
-}
+      while (radius == 0) {
+        BMP_D bmpTemp;
+        bmpTemp.CreateBMP(xS, yS, 300, 300);
+        GetRadius(bmpTemp);
+        bmpTemp.DeleteBMP();
+        bmpSize = radius * 2 + bmpPadding * 2;
+        bmpHalfSize = bmpSize / 2;
+        bmp.CreateBMP(xS - bmpPadding, yS - bmpPadding, bmpSize, bmpSize);
+      }
 
-ROUND circle;
-HHOOK hHook;
+      if (WhitePart(bmp, rad)) {
+        if (RedPart(bmp, rad)) {
+          cout << "Sending Mouse X2" << endl;
+          SendKey(0x06);
+          cout << "Delay: " << QPC(0) << " milliseconds" << endl;
+        }
+      }
 
-int main() {
-	HDC hdc = GetDC(NULL);
-	PDATA pixel;
-	BMP bmp;
+      //bmp.svBMP();
 
-	bmp.doMagic();
-	QPC(1);
+      cout << "============================================================================" << endl;
+      Sleep(1500);
+    }
+  }
 
-	while (1) {
-		pixel = GetPixelData(hdc, 800, 440);
-		if (pixel.r == 0 and pixel.g == 0 and pixel.b == 0) {
-			cout << "Skillcheck found:" << pixel.r << " " << pixel.g << " " << pixel.b << endl;
-			cout << QPC(0) << "ms" << endl;
+  system("pause");
 
-			double rad;
-
-			if (whitePart(bmp, &rad)) {
-				if (redPart(bmp, rad)) {
-					QPC(1);
-					SendKey(0x06);
-					cout << "Sending Mouse X2" << endl;
-					cout << QPC(0) << "ms" << endl;
-				}
-			}
-
-			Sleep(1500);
-		}
-	}
-
-	system("pause");
-
-	return 0;
+  return 0;
 }
---
+++ ImageSearch.h
@@ -0,0 +1,272 @@
+#include <iostream>
+#include <windows.h>
+
+#pragma once
+
+using namespace std;
+
+string ErrLvl = "";
+
+int _tol = 0;
+int _left, _right, _top, _bottom;
+int _ignoreColor = 0xcccccccc;
+
+class BMP {
+public:
+  HDC hdc, hdcTemp;
+  BITMAPINFO bitmapInfo;
+  HBITMAP hBitmap;
+  int MAX_WIDTH, MAX_HEIGHT, MAP_SIZE, BIT_ALLOC;
+  int* pixelMap;
+  unsigned char* bitPointer;
+
+  BMP() {
+	BIT_ALLOC = 4;
+	MAX_WIDTH = GetSystemMetrics(SM_CXSCREEN);
+	MAX_HEIGHT = GetSystemMetrics(SM_CYSCREEN);
+	MAP_SIZE = MAX_WIDTH * MAX_HEIGHT * BIT_ALLOC;
+
+	hdc = GetDC(0);
+	if (!hdc) return;
+
+	bitmapInfo.bmiHeader.biSize = sizeof(bitmapInfo.bmiHeader);
+	bitmapInfo.bmiHeader.biWidth = MAX_WIDTH;
+	bitmapInfo.bmiHeader.biHeight = -MAX_HEIGHT;
+	bitmapInfo.bmiHeader.biPlanes = 1;
+	bitmapInfo.bmiHeader.biBitCount = BIT_ALLOC * 8;
+	bitmapInfo.bmiHeader.biCompression = BI_RGB;
+	bitmapInfo.bmiHeader.biSizeImage = MAP_SIZE;
+	bitmapInfo.bmiHeader.biClrUsed = 0;
+	bitmapInfo.bmiHeader.biClrImportant = 0;
+
+	hBitmap = CreateDIBSection(hdc, &bitmapInfo, DIB_RGB_COLORS, (void**)(&bitPointer), NULL, 0);
+
+	if (!hBitmap) return;
+
+	hdcTemp = CreateCompatibleDC(NULL);
+
+	if (!hdcTemp) return;
+
+	SelectObject(hdcTemp, hBitmap);
+
+	BitBlt(hdcTemp, 0, 0, MAX_WIDTH, MAX_HEIGHT, hdc, 0, 0, SRCCOPY);
+
+	ConvertMap();
+
+	return;
+  }
+
+  void DeleteBMP() {
+	delete[] pixelMap;
+
+	return;
+  }
+
+private:
+  void ConvertMap() {
+	int buffer;
+
+	pixelMap = new int[MAP_SIZE / BIT_ALLOC];
+
+	for (int i = 0; i < MAP_SIZE; i += BIT_ALLOC) {
+	  buffer = int(bitPointer[i + 2] << 16 & 0xff0000);
+	  buffer += int(bitPointer[i + 1] << 8 & 0xff00);
+	  buffer += int(bitPointer[i + 0] & 0xff);
+	  pixelMap[i / BIT_ALLOC] = buffer;
+	}
+
+	DeleteObject(hBitmap);
+	ReleaseDC(NULL, hdc);
+	DeleteDC(hdc);
+	DeleteDC(hdcTemp);
+
+	return;
+  }
+};
+
+class BMP_IMAGE {
+public:
+  HDC hdc;
+  BITMAP bitmap;
+  HBITMAP hBitmap;
+  BITMAPINFOHEADER bitmapInfo;
+  int MAX_WIDTH, MAX_HEIGHT, MAP_SIZE, BIT_ALLOC;
+  int* pixelMap;
+  unsigned char* bitPointer;
+
+  BMP_IMAGE(HBITMAP bmp) {
+	if (!bmp) return;
+
+	hBitmap = bmp;
+	GetObject(hBitmap, sizeof(bitmap), &bitmap);
+
+	if (!bitmap.bmWidth) return;
+
+	MAX_WIDTH = bitmap.bmWidth;
+	MAX_HEIGHT = bitmap.bmHeight;
+	BIT_ALLOC = 4;
+	MAP_SIZE = MAX_WIDTH * MAX_HEIGHT * BIT_ALLOC;
+
+	bitmapInfo.biSize = sizeof(BITMAPINFOHEADER);
+	bitmapInfo.biWidth = MAX_WIDTH;
+	bitmapInfo.biHeight = -MAX_HEIGHT;
+	bitmapInfo.biPlanes = 1;
+	bitmapInfo.biBitCount = BIT_ALLOC * 8;
+	bitmapInfo.biCompression = BI_RGB;
+	bitmapInfo.biSizeImage = MAP_SIZE;
+	bitmapInfo.biClrUsed = 0;
+	bitmapInfo.biClrImportant = 0;
+
+	hdc = CreateCompatibleDC(NULL);
+
+	bitPointer = new unsigned char[MAP_SIZE];
+
+	if (!GetDIBits(hdc, hBitmap, 0, MAX_HEIGHT, bitPointer, (BITMAPINFO*)&bitmapInfo, DIB_RGB_COLORS)) return;
+
+	ConvertMap();
+
+	return;
+  }
+
+  void DeleteBMP() {
+	delete[] pixelMap;
+
+	return;
+  }
+
+private:
+  void ConvertMap() {
+	int buffer;
+
+	pixelMap = new int[MAP_SIZE / BIT_ALLOC];
+
+	for (int i = 0; i < MAP_SIZE; i += BIT_ALLOC) {
+	  buffer = int(bitPointer[i + 2] << 16 & 0xff0000);
+	  buffer += int(bitPointer[i + 1] << 8 & 0xff00);
+	  buffer += int(bitPointer[i + 0] & 0xff);
+	  pixelMap[i / BIT_ALLOC] = buffer;
+	}
+
+	DeleteObject(hBitmap);
+	DeleteDC(hdc);
+	delete[] bitPointer;
+
+	return;
+  }
+};
+
+
+
+HBITMAP LoadPicture(string img) {
+  LPCSTR path = img.c_str();
+
+  HBITMAP bmp = (HBITMAP)LoadImageA(NULL, path, IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+
+  if (!bmp) {
+	ErrLvl = "2: Could not load bitmap.";
+	return 0;
+  }
+
+  return bmp;
+}
+
+int CompareImage(BMP bmp, BMP_IMAGE bmpImage, int x, int y) {
+  int* screen = bmp.pixelMap;
+  int* image = bmpImage.pixelMap;
+  int height, sHeight;
+  for (int i = bmpImage.MAX_HEIGHT - 1; i >= 0; i--) {
+	height = i * bmpImage.MAX_WIDTH;
+	sHeight = (y + i) * bmp.MAX_WIDTH;
+	for (int j = bmpImage.MAX_WIDTH - 1; j >= 0; j--) {
+	  if (x + j > _right) return 0;
+	  if (y + i > _bottom) return 0;
+
+	  if (image[height + j] == _ignoreColor) continue;
+
+	  if (_tol == 0) {
+		if (image[height + j] != screen[sHeight + x + j]) return 0;
+	  }
+	  else {
+		if (abs((image[height + j] >> 16) - (screen[sHeight + x + j] >> 16)) > _tol or
+		  abs((image[height + j] >> 8 & 0xff) - (screen[sHeight + x + j] >> 8 & 0xff)) > _tol or
+		  abs((image[height + j] & 0xff) - (screen[sHeight + x + j] & 0xff)) > _tol) return 0;
+	  }
+	}
+  }
+
+  return 1;
+}
+
+int FindPixelMatch(BMP bmp, BMP_IMAGE bmpImage) {
+  int* screen = bmp.pixelMap;
+  int* image = bmpImage.pixelMap;
+
+  if (_right == GetSystemMetrics(SM_CXSCREEN)) _right -= 1;
+  if (_bottom == GetSystemMetrics(SM_CYSCREEN)) _bottom -= 1;
+  int sHeight;
+  int scanTrans = 0;
+  if (image[0] == _ignoreColor) scanTrans = 1;
+  for (int i = _top; i <= _bottom; i++) {
+	sHeight = i * bmp.MAX_WIDTH;
+	for (int j = _left; j <= _right; j++) {
+	  if (scanTrans) {
+		if (CompareImage(bmp, bmpImage, j, i)) return sHeight + j;
+	  }
+	  if (_tol == 0) {
+		if (image[0] == screen[sHeight + j]) {
+		  if (CompareImage(bmp, bmpImage, j, i)) return sHeight + j;
+		}
+	  }
+	  else {
+		if (abs((image[0] >> 16) - (screen[sHeight + j] >> 16)) <= _tol and
+		  abs((image[0] >> 8 & 0xff) - (screen[sHeight + j] >> 8 & 0xff)) <= _tol and
+		  abs((image[0] & 0xff) - (screen[sHeight + j] & 0xff)) <= _tol) {
+		  if (CompareImage(bmp, bmpImage, j, i)) return sHeight + j;
+		}
+	  }
+	}
+  }
+
+  return -1;
+}
+
+int FindImage(BMP bmp, BMP_IMAGE bmpImage, int& x, int& y) {
+  int position = FindPixelMatch(bmp, bmpImage);
+
+  if (position > -1) {
+	x = position % bmp.MAX_WIDTH;
+	y = position / bmp.MAX_WIDTH;
+	return 1;
+  }
+
+  return position;
+}
+
+int ImageSearch(int& x, int& y, int left, int top, int right, int bottom, string imgPath = "", int tol = 0, int ignoreColor = 0xcccccccc) {
+  if (imgPath != "") {
+	_left = left;
+	_right = right < 1 ? GetSystemMetrics(SM_CXSCREEN) + right : right;
+	_top = top;
+	_bottom = bottom < 1 ? GetSystemMetrics(SM_CYSCREEN) + bottom : bottom;
+	_tol = tol;
+	_ignoreColor = ignoreColor;
+
+	if (_tol < 0) return 0;
+
+	BMP screen = BMP();
+
+	BMP_IMAGE image = BMP_IMAGE(LoadPicture(imgPath));
+
+	int success = FindImage(screen, image, x, y);
+
+	screen.DeleteBMP();
+	image.DeleteBMP();
+
+	if (success == 1) return 1;
+
+	return 0;
+  }
+
+  ErrLvl = "2: Image path not specified.";
+  return 2;
+}
